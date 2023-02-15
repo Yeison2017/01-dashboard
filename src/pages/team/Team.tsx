11337cf24c5ff2c94573ded94149a92d9480d58c
yeison2017 <yha.big@gmail.com>
2023-02-15 16:05:42 -0500

    Se refactoriza la page Team

--- src/pages/team/Team.tsx
+++ src/pages/team/Team.tsx
@@ -1,30 +1,11 @@
-import { Box, Typography } from "@mui/material";
-import { DataGrid, GridColumns } from "@mui/x-data-grid";
-import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
-import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
-import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import { Box } from "@mui/material";
 
 import { Header } from "../../components";
-import { mockDataTeam } from "../../data";
 import { useTheme } from "../../hooks";
 import { IStyles, ITokens } from "../../interfaces";
+import TeamTable from "./TeamTable";
 
-interface IStylesProps {
-  access?: string;
-  colors?: ITokens;
-}
-
-const styles = ({ access, colors }: IStylesProps): IStyles => ({
-  accessLevel: {
-    width: "60%",
-    m: "0 auto",
-    p: "5px",
-    display: "flex",
-    justifyContent: "center",
-    borderRadius: "4px",
-    backgroundColor:
-      access === "admin" ? colors?.greenAccent[600] : colors?.greenAccent[700],
-  },
+const styles = (colors?: ITokens): IStyles => ({
   container: {
     m: "20px",
   },
@@ -57,56 +38,11 @@ const styles = ({ access, colors }: IStylesProps): IStyles => ({
 const Team = () => {
   const { colors } = useTheme();
 
-  const columns: GridColumns = [
-    { field: "id", headerName: "ID" },
-    {
-      field: "name",
-      headerName: "Name",
-      flex: 1,
-      cellClassName: "name-column-cell",
-    },
-    {
-      field: "age",
-      headerName: "Age",
-      type: "number",
-      headerAlign: "left",
-      align: "left",
-    },
-    {
-      field: "phone",
-      headerName: "Phone Number",
-      flex: 1,
-    },
-    {
-      field: "email",
-      headerName: "Email",
-      flex: 1,
-    },
-    {
-      field: "access",
-      headerName: "Access Level",
-      flex: 1,
-      renderCell: ({ row: { access } }) => {
-        return (
-          <Box sx={styles({ access, colors }).accessLevel}>
-            {access === "admin" && <AdminPanelSettingsOutlinedIcon />}
-            {access === "manager" && <SecurityOutlinedIcon />}
-            {access === "user" && <LockOpenOutlinedIcon />}
-
-            <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
-              {access}
-            </Typography>
-          </Box>
-        );
-      },
-    },
-  ];
-
   return (
-    <Box sx={styles({}).container}>
+    <Box sx={styles().container}>
       <Header title="TEAM" subtitle="Managing the Team Members" />
-      <Box sx={styles({ colors }).containerGrid}>
-        <DataGrid rows={mockDataTeam} columns={columns} />
+      <Box sx={styles(colors).containerGrid}>
+        <TeamTable />
       </Box>
     </Box>
   );
---
+++ src/pages/team/TeamTable.tsx
@@ -0,0 +1,80 @@
+import { Box, Typography } from "@mui/material";
+import { DataGrid, GridColumns } from "@mui/x-data-grid";
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+
+import { mockDataTeam } from "../../data";
+import { useTheme } from "../../hooks";
+import { IStyles, ITokens } from "../../interfaces";
+
+interface IStylesProps {
+  access?: string;
+  colors?: ITokens;
+}
+
+const styles = ({ access, colors }: IStylesProps): IStyles => ({
+  accessLevel: {
+    width: "60%",
+    m: "0 auto",
+    p: "5px",
+    display: "flex",
+    justifyContent: "center",
+    borderRadius: "4px",
+    backgroundColor:
+      access === "admin" ? colors?.greenAccent[600] : colors?.greenAccent[700],
+  },
+});
+
+const TeamTable = () => {
+  const { colors } = useTheme();
+
+  const columns: GridColumns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column-cell",
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "access",
+      headerName: "Access Level",
+      flex: 1,
+      renderCell: ({ row: { access } }) => {
+        return (
+          <Box sx={styles({ access, colors }).accessLevel}>
+            {access === "admin" && <AdminPanelSettingsOutlinedIcon />}
+            {access === "manager" && <SecurityOutlinedIcon />}
+            {access === "user" && <LockOpenOutlinedIcon />}
+
+            <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
+              {access}
+            </Typography>
+          </Box>
+        );
+      },
+    },
+  ];
+
+  return <DataGrid rows={mockDataTeam} columns={columns} />;
+};
+
+export default TeamTable;
